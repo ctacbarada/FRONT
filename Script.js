8762659d4aad1f5d86154d532b714fd91a7c6406
Stanislav <stas.sorochan@gmail.com>
2021-11-04 00:27:39 +0100

    update

---
+++ Script.js
@@ -0,0 +1,8 @@
+//let newElemet = document.createElement('    ');
+
+//document.body.appendChild(newElemet);
+
+function addTextToElement() {
+    var x = document.getElementById('input-text').value;
+    document.getElementById('div').innerHTML = x;
+}
